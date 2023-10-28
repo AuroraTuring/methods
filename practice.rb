a8b7bdddeab133e5856d6b04d5a6c742600e40dc
Aurora Sanderson <AuroraTuring@gmail.com>
2023-10-28 14:04:16 -0600

    Move file

---
+++ practice.rb
@@ -0,0 +1,9 @@
+def greet_an_animal(name)
+  puts "Hello there mister #{name}!"
+end
+
+greet_an_animal("Tiger")
+
+
+
+# Write a method named get_age. This method should declare one parameter, an Integer, representing a birth year. When called, the method should calculate the person's approximate age. Let's not worry about birth month or day. 😉 The method should return an Integer representing the person's age. Call the method several times with different arguments and store the return values in variables. Print a sentence that says, "You have lived ___ years of life!" with each variable interpolated into the String.
